e99d5693f067f264a5e640f3c454054f6d7557ab
chima <chimaemekaiheonu@gmail.com>
2024-06-25 14:30:34 +0100

    new change

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -13,7 +13,7 @@ const Model = () => {
     <section className="common-padding">
         <div className="screen-max-width">
             <h1 id="heading" className="section-heading">
-                Take a closer look
+                Take a closer look.
             </h1>
             <div className="flex flex-col items-center mt-5">
                 <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
